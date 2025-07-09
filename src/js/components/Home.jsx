ed209cddefa518893e2109ad40664744212fcf0b
JoseLuGeek <joseluisrobleslao10@gmail.com>
2025-07-09 09:50:07 +0000

    Contador Simple

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,46 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import { Digit } from "./digit.jsx";
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const [timer, setTimer] = useState(0)
+
+
+	useEffect(() => {
+
+		setTimeout(() => {
+			setTimer(value => value + 1)
+		}, 1000)
+
+	}, [timer])
+
+	return (
+		<main className="=text-center">
+			<section className="counter-holder">
+				<Digit number={<span className="fa fa-clock"></span>} />
+				<div className="6">
+					<Digit number={Math.floor(timer / 100000) % 10} />
+				</div>
+				<div className="5">
+					<Digit number={Math.floor(timer / 10000) % 10} />
+				</div>
+				<div className="4">
+					<Digit number={Math.floor(timer / 1000) % 10} />
+				</div>
+				<div className="3">
+					<Digit number={Math.floor(timer / 100) % 10} />
+				</div>
+				<div className="2">
+					<Digit number={Math.floor(timer / 10) % 10} />
+				</div>
+				<div className="1">
+					<Digit number={Math.floor(timer % 10)} />
+				</div>
+			</section>
+		</main>
 	);
 };
 
---
+++ src/js/components/digit.jsx
@@ -0,0 +1,4 @@
+import react from "react";
+export const Digit = ({number}) => <div className="digit">{number}</div>
+
+    
